4a7b7ac0c63c4f5336e96120942d92a714dad7b4
BrianMBowden <brianmbowden@live.ca>
2021-12-07 23:01:12 -0700

    created ScratchLinked package, added Node class

---
+++ src/Node.java
@@ -0,0 +1,14 @@
+package ScratchLinked;
+
+public class Node<T>{
+
+    private T data;
+    private Node<t> next;
+
+    public Node(T data){
+        this.data = data;
+        this.next = null;
+
+    }
+
+}
